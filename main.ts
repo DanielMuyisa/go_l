0141eadd95bee51a3e9d784730d8c77a9962336b
Daniel Muyisa <danielmuyisa37@gmail.com>
2023-03-11 16:14:40 +0100

    proptype init

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 const rdUser: Function = () => {
 
@@ -10,7 +10,7 @@ function Main(props) {
   )
 }
 
-// Main.propTypes = {}
+Main.propTypes = {}
 
 export default Main
 
