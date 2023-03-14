07198fe3317b6b658998b83dbad7affdc185d8a2
Daniel Muyisa <danielmuyisa37@gmail.com>
2023-03-14 22:52:31 +0100

    tuble (readonly)

--- arry/simp.ts
+++ arry/simp.ts
@@ -6,8 +6,8 @@ let head: number = arr[0]
 
 console.log(head);
 
-// tuble
-let list: [number, boolean, string]
+// tuble (readonly)
+let list: readonly [number, boolean, string]
 list = [3,false,'s']
-list.push(3,true,"string")
+// list.push(3,true,"string")  // error
 console.log(list)
